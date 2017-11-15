945e7cdadc8d8e8d643e848f4f6f243a9af3511d
Nicolas Escalante <nlante@gmail.com>
2017-11-15 17:25:39 -0300

    add del tag

--- server/sanitize.js
+++ server/sanitize.js
@@ -1,7 +1,7 @@
 var sanitize = require('sanitize-html');
 var config = {
   allowedTags: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
-    'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div', 'table', 'thead',
+    'li', 'b', 'i', 'strong', 'em', 'del', 'strike', 'code', 'hr', 'br', 'div', 'table', 'thead',
     'caption', 'tbody', 'tr', 'th', 'td', 'pre'],
   allowedAttributes: {
     a: ['href', 'title'],
