e41b62a09bc5feacde1179956a4099b8d5973a81
Nicolas Walter Araya <nwaraya@gmail.com>
2022-04-13 17:01:18 -0300

    Update sanitize.js

--- server/sanitize.js
+++ server/sanitize.js
@@ -1,6 +1,6 @@
 var sanitize = require('sanitize-html');
 var config = {
-  allowedTags: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
+  allowedTags: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol', 'span',
     'li', 'b', 'i', 'strong', 'em', 'del', 'strike', 'code', 'hr', 'br', 'div', 'table', 'thead',
     'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img'],
   allowedAttributes: {
